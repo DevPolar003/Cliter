3817ff80187677dd521627a9f8fdffa07f9810cc
The oldest dream <joaorivaroli.m@gmail.com>
2026-06-10 23:01:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ os.system("cls")
 nome_arquivo = "dados.txt"
 
 with open(nome_arquivo, 'a+', encoding='utf-8') as arquivo:
-    
+
     while True:
         print("\n--- MENU DE OPÇÕES ---")
         print("0 - SAIR")
@@ -19,89 +19,127 @@ with open(nome_arquivo, 'a+', encoding='utf-8') as arquivo:
 
         try:
             option = int(input("Selecione uma das opções: "))
+
         except ValueError:
-            print("Por favor, digite um número válido.")
+            print("digite um número valido.")
+            continue
+        except:
+            print("chame o programador chefe")
             continue
 
         match option:
 
-            case 0:
-                print("Saindo....")   
-                break
-                
-            case 1:
-                dados = input("Digite o texto: ")
-                arquivo.write(dados + "\n")
-                arquivo.flush() 
-                print("Linha gravada com sucesso!")
+                case 0:
+                    print("Saindo....")
+                    break
 
-            case 2:
-                palavras = []
-                qtd_de_palavra = int(input("digite quantas palavras voce quer escrer"))
-                for i in range (qtd_de_palavra):
-                      frases = input("digite a palavra: ")
-                      palavras.append(frases + "\n")
-                arquivo.writelines(palavras)
-            case 3:
-                print(f"\n--- {nome_arquivo} ---")
-                arquivo.seek(0)
-                linhas = arquivo.readlines()
-                
-                if not linhas:
-                    print(f"Não há nada no arquivo {nome_arquivo}")
-                else:
-                    for linha in linhas:
-                        print(linha.strip())
+                case 1:
+                    dados = input("Digite o texto: ")
+                    arquivo.write(dados + "\n")
+                    arquivo.flush()
+                    print("Linha gravada com sucesso!")
 
-            case 4:
+                case 2:
+                    try:
+                        palavras = []
+
+                        qtd_de_palavra = int(input("Digite quantas linhas voce quer escrever: "))
+
+                        for i in range(qtd_de_palavra):
+                            frases = input("Digite a palavra: ")
+                            palavras.append(frases + "\n")
+
+                    except ValueError:
+                        print("Digite apenas números!")
+
+                    except:
+                        print("chame o programador chefe")
+
+
+                    else:
+                        arquivo.writelines(palavras)
+                        print("Linhas gravadas com sucesso!")
+                        arquivo.flush()
+
+
+                case 3:
+                    print(f"\n--- {nome_arquivo} ---")
+
                     arquivo.seek(0)
                     linhas = arquivo.readlines()
-                    linha_desejada = int(input(f"Defina a linha que quer ver (1 a {len(linhas)}): "))
-                    if 0 <= linha_desejada <= len(linhas):
-                            print(f"Conteúdo: {linhas[linha_desejada - 1]}")
+
+                    if not linhas:
+                        print(f"Não há nada no arquivo {nome_arquivo}")
                     else:
-                            print("Essa linha não existe no arquivo!")
-            case 5:
-                arquivo.seek(0)
-                texto = "".join(arquivo)
-                qtd_palavras = texto.split()
-                print(f"Quantidade total de palavras: {len(qtd_palavras)}")
+                        for linha in linhas:
+                            print(linha.strip())
 
-            case 6:
-               
-                qtd_letras = int(input("Quantidade de letras: ")) 
-                count = 0
-                     
-                arquivo.seek(0)
-                texto = "".join(arquivo)
-        
-                qtd_palavras = texto.split()
-        
-                for palavra in qtd_palavras:
-                    if len(palavra) == qtd_letras:
-                        count += 1
-                            
-                print(f"Palavras com {qtd_letras} letras: {count}")
+                case 4:
+                    try:
+                        arquivo.seek(0)
+                        linhas = arquivo.readlines()
 
-            case 7:
-                pass
+                        linha_desejada = int(input(f"Defina a linha que quer ver (1 a {len(linhas)}): "))
 
-            case 8:
-                arquivo.seek(0)
-                palavras = arquivo.read()
-                palavras2 = palavras.split()
-                n = int(input("digite o numero de caracteres: "))
-                for palavras2 in palavras2:
-                    if len(palavras) == n:
-                        count = count + 1
-                print(f"existem {count} palavras com {n}")
-           
-            while True:
-                qtd_palavras = []
-                a = input("Digite uma palavra ou 'sair' para terminar: ").strip().lower()
-                if a == "sair":
-                    break
-                    
-                qtd_palavras.append(a)
-               
-                print(f"Quantidade de palavras até agora: {len(qtd_palavras)}")
+                        if 1 <= linha_desejada <= len(linhas):
+                            print(
+                                f"Conteúdo: {linhas[linha_desejada - 1]}"
+                            )
+                        else:
+                            print("Essa linha não existe!")
+
+                    except ValueError:
+                        print("Digite apenas números!")
+                    except:
+                        print("chame o programador chefe")
+
+                    else:
+                        print("Consulta realizada com sucesso!")
+
+                case 5:
+                    arquivo.seek(0)
+                    texto = "".join(arquivo)
+                    qtd_palavras = texto.split()
+
+                    print(f"Quantidade total de palavras: {len(qtd_palavras)}")
+
+                case 6:
+                    arquivo.seek(0)
+                    texto = arquivo.read()
+                    print(f"Quantidade de caracteres: {len(texto)}")
+
+                case 7:
+                    try:
+                        count = 0
+
+                        arquivo.seek(0)
+
+                        palavras = arquivo.read()
+                        palavras2 = palavras.split()
+
+                        n = int(
+                            input("Digite o numero de caracteres: "))
+
+                        for palavra in palavras2:
+                            if len(palavra) == n:
+                                count += 1
+
+                    except ValueError:
+                        print("Digite apenas números!")
+
+                    else:
+                        print(f"Existem {count} palavras com {n} caracteres")
+
+                case 8:
+                    arquivo.seek(0)
+
+                    texto = arquivo.read().lower()
+
+                    palavra = input("Digite a palavra: ").lower()
+
+                    quantidade = texto.split().count(palavra)
+
+                    print(f"A palavra '{palavra}' aparece {quantidade} vezes no arquivo.")
+
+                case _:
+                    print("Opção inválida!")
